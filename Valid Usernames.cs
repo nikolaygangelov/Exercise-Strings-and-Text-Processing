4b4f022519173d56530853d1402410e6a9804b46
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-11-10 09:24:28 +0200

    Add files via upload

---
+++ Valid Usernames.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Valid_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < usernames.Length; i++)
+            {
+                bool isValid = true;
+                string currentUsername = usernames[i];
+                if (currentUsername.Length < 3 || currentUsername.Length > 16)
+                {
+                    continue;
+                }
+
+                for (int j = 0; j < currentUsername.Length; j++)
+                {
+
+                    if (!char.IsLetterOrDigit(currentUsername[j]))
+                    {
+                        
+                        if ((int)currentUsername[j] != 95 && (int)currentUsername[j] != 45)
+                        {
+                            isValid = false;
+                            
+                        }
+                    }
+                }
+
+                if (isValid)
+                {
+                    Console.WriteLine(currentUsername);
+                }
+            }
+        }
+    }
+}
