a8e61570974d001b48babe4bf0392b032b60a0c1
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-11-10 21:20:54 +0200

    Add files via upload

---
+++ Extract File.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _3.__Extract_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Console.ReadLine();
+            
+            int startFileIndex = filePath.LastIndexOf('\\') + 1;
+            int startFileExtensionIndex = filePath.IndexOf('.') + 1;
+            int fileNameLength = startFileExtensionIndex - 1 - startFileIndex;
+
+            string fileName = filePath.Substring(startFileIndex, fileNameLength);
+            string fileExtension = filePath.Substring(startFileExtensionIndex);
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {fileExtension}");
+        }
+    }
+}
