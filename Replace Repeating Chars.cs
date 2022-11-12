c395a42f8ff328b6c9535358968b239cf48427aa
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-11-12 20:20:15 +0200

    Add files via upload

---
+++ Replace Repeating Chars.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _6.__Replace_Repeating_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < text.Length; i++)//aab
+            {
+                char currentLetter = text[i];
+                Console.Write(currentLetter);
+                if (i == text.Length - 1)
+                {
+                    break;
+                }
+                char nextLetter = text[i + 1];
+                while (currentLetter == nextLetter)//a=a
+                {
+                    text = text.Remove(i, 1);//qqqwerqwecccwd
+                    currentLetter = text[i];
+                    if (i == text.Length - 1)
+                    {
+                        break;
+                    }
+                    nextLetter = text[i + 1];
+                }
+            }
+        }
+    }
+}
