662b931b16755eda7e2f4792cd0d72888ccab280
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-11-12 20:19:21 +0200

    Add files via upload

---
+++ Caesar Cipher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _4.__Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currentChar = text[i];
+                int newUnicode = (int)currentChar + 3;
+
+                char newChar = (char)newUnicode;
+
+                Console.Write(newChar);
+            }
+
+        }
+    }
+}
