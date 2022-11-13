384d1ea4d2988132087a77402d4a15411b25d14e
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-11-13 17:28:05 +0200

    Add files via upload

---
+++ String Explosion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _7.__String_Explosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            int explosionStrength = 0;
+            for (int i = 0; i < text.Length - 1; i++)
+            {
+                char currentLetter = text[i];
+                char nextLetter = text[i + 1];
+
+                if (currentLetter == '>')
+                {
+                    explosionStrength += int.Parse(nextLetter.ToString());
+                    while (explosionStrength > 0 && nextLetter != '>')
+                    {
+                        text = text.Remove(i + 1, 1);
+                        explosionStrength--;
+                        if (i == text.Length - 1)
+                        {
+                            break;
+                        }
+                        nextLetter = text[i + 1];
+                    }
+                    
+                }
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
